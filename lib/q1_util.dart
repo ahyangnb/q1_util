5ce28fbaa08e150c1a6395d899b23df69dda3453
ahyangnb <zonggeu@gmail.com>
2024-10-08 08:15:28 +0800

    firstWhereOrNull

--- lib/q1_util.dart
+++ lib/q1_util.dart
@@ -29,3 +29,13 @@ class Q1Utils {
 extension Q1BoolExtension on bool {
   bool not() => !this;
 }
+
+extension FirstWhereExt<T> on List<T> {
+  /// The first element satisfying [test], or `null` if there are none.
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (var element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
+}
